03d7aa835a50662055f777c6587738921e952d03
Ram <rambo300502@gmail.com>
2024-10-25 15:02:43 +0530

    Initial Commit

---
+++ learnswiftuiin100days/learnswiftuiin100daysApp.swift
@@ -0,0 +1,17 @@
+//
+//  learnswiftuiin100daysApp.swift
+//  learnswiftuiin100days
+//
+//  Created by sagar on 10/25/24.
+//
+
+import SwiftUI
+
+@main
+struct learnswiftuiin100daysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
